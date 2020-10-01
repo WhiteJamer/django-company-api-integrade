ca4c126bf298db140597fb8b5bd5d5892ce08ef7
whitejamer <mihailkuzin.job@yande.ru>
2020-10-01 17:11:18 +0300

    Complete docker-compose configuration for dev

--- core/settings.py
+++ core/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': os.getenv('POSTGRES_DB'),
-        'HOST': os.getenv('POSTGRES_HOST'),
+        'HOST': 'db',
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'PORT': os.getenv('POSTGRES_PORT'),
